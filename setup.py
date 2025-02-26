d022f7070eed24c657dac64f9e56c9707e227b6c
XL636 <l.xinghong@wustl.edu>
2025-02-26 11:46:05 -0600

    update more file

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='XINGHONG LI',
     author_email='lxh04112002@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","langchain-openai","langchain-community"],
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2","langchain_openai","langchain_community"],
     packages=find_packages()
 )
